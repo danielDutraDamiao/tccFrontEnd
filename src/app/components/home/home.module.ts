773e5e55ecef87c766673127c5d03124d402525f
fcamilayumi <265312@unifio.edu.br>
2023-06-27 19:08:24 -0300

    commit1-telahome
    ..

---
+++ src/app/components/home/home.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HomeRoutingModule } from './home-routing.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    HomeRoutingModule
+    
+  ]
+})
+export class HomeModule { }
